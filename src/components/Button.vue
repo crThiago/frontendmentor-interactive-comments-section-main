1c29d51cb8039a6c94c801af0bedc1ff4b243eff
Thiago R Costa <thiago_8777@hotmail.com>
2023-01-26 07:26:10 -0300

    adicionando atributos role e title

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="px-4 py-2 bg-moderate-blue hover:bg-light-grayish-blue uppercase text-white rounded-lg font-semibold">
+  <button role="button" class="px-4 py-2 bg-moderate-blue hover:bg-light-grayish-blue uppercase text-white rounded-lg font-semibold">
     <slot />
   </button>
 </template>
